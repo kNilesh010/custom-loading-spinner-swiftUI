e71286c6282f3bded7c0aaaf94ba3c0056af0757
Nilesh Kumar <k.nilesh010@gmail.com>
2022-06-17 11:31:19 +0530

    Initial Commit

---
+++ Loading-Spinner-SwiftUI/Loading_Spinner_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Loading_Spinner_SwiftUIApp.swift
+//  Loading-Spinner-SwiftUI
+//
+//  Created by Nilesh Kumar on 17/06/22.
+//
+
+import SwiftUI
+
+@main
+struct Loading_Spinner_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
